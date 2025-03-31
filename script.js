9f8d387e5061be387ffc83ce467d15bfcdb0483e
Robert Benson <robtwopointoh@gmail.com>
2025-03-30 18:47:04 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    window.open('https://dfe922ucr7iz-m74q85osrgn7v.hop.clickbank.net')
+}
+
+function forward(){
+    window.open('https://dfe922ucr7iz-m74q85osrgn7v.hop.clickbank.net')
+}
